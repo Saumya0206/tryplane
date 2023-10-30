d63d11022f9c3c2f7fc7c1f48dc6b9708b24c8c4
Ubuntu <vaish.1@iitj.ac.in>
2023-10-30 11:26:55 +0000

    first commit

--- apiserver/manage.py
+++ apiserver/manage.py
@@ -1,5 +1,21 @@
 #!/usr/bin/env python
 import os
+ 
+# os.environ["ATLAS_BROKER_HOST"] = "ws://localhost"
+ 
+# os.environ["ATLAS_BROKER_PORT"] = "8092"
+ 
+try:
+ 
+    import heimdall
+ 
+    heimdall.start(apikey="b0265c61-8ff5-4418-a354-a154197e4d1f") # if this is your new api key
+ 
+except ImportError as e:
+ 
+     print("Error importing Heimdall: ", e)
+
+import os
 import sys
 
 if __name__ == '__main__':
